0d8353685a694fd94eebc975d20d2582077ffcdc
Данил Гадельшин <gadik2002@gmail.com>
2023-07-10 18:03:00 +0500

    Exercise 15 success

---
+++ Exercise 15/Program.cs
@@ -0,0 +1,14 @@
+﻿int Prompt(string msg)
+{
+    Console.Write($"{msg}");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int number = Prompt("Введите число дня недели от 1 до 7: ");
+string ss = "Это не день недели";
+string check(int number)
+{
+    if (number > 5 && number < 8) ss = "Это выходной день";
+    if (number >= 1 && number < 6) ss = "Это будний день";
+    return ss;
+}
+Console.WriteLine(check(number));
