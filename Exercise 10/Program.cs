85359cd20728f89c662e4887a20e5efa2fe11a29
Данил Гадельшин <gadik2002@gmail.com>
2023-07-10 17:41:42 +0500

    Exercise 10 success

---
+++ Exercise 10/Program.cs
@@ -0,0 +1,13 @@
+﻿int Prompt(string msg)
+{
+    Console.Write($"{msg}");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int number = Prompt("Введите трехзначное число: ");
+if (number >=100 && number <1000)
+{
+    int mid = ((number / 10) % 10);
+    Console.WriteLine($"Вторая цифра числа {number} является {mid}");
+}
+else
+Console.WriteLine("Это число не трехзначное");
