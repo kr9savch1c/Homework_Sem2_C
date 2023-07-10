6e992b381137902865ed69546c5d6e9d17d2ff48
Данил Гадельшин <gadik2002@gmail.com>
2023-07-10 17:50:27 +0500

    Exercise 13 success

---
+++ Exercise 13/Program.cs
@@ -0,0 +1,25 @@
+﻿int Prompt(string msg)
+{
+    System.Console.Write($"{msg}");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int number = Prompt("Введите число: ");
+int fnumber(int number)
+{
+    while (number > 999)
+{
+    number /= 10;
+}
+return number % 10;
+}
+    bool check(int number)
+{
+    if (number < 100)
+    return false;
+    else return true;
+}
+
+if (check(number) != true)
+Console.WriteLine("Третьей цифры нет");
+else
+Console.WriteLine($"Третья цифра числа {number} является {fnumber(number)}");
